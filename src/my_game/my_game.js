956446ae0baad6574f064a53d97e3de60203e943
Mama-Thomas <tmama20012001@gmail.com>
2023-01-21 20:22:18 -0800

    implemented the codes to draw a square of random color on the screen when the space bar is clicked, in addition, it has functionalities for other keys aswell

--- src/my_game/my_game.js
+++ src/my_game/my_game.js
@@ -15,18 +15,22 @@ class MyGame {
   constructor() {
     // variables for the squares
     this.mWhiteSq = null; // these are the Renderable objects
+
+    //red square
     this.mRedSq = null;
 
     // The camera to view the scene
     this.mCamera = null;
+
+    this.spacebarPressed = false;
   }
 
   init() {
     // Step A: set up the cameras
     this.mCamera = new engine.Camera(
-      vec2.fromValues(20, 60), // position of the camera
-      20, // width of camera
-      [20, 40, 600, 300] // viewport (orgX, orgY, width, height)
+      vec2.fromValues(50,37.5), // position of the camera
+      100, // width of camera
+      [0, 0, 640, 480] // viewport (orgX, orgY, width, height)
     );
     this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
     // sets the background to gray
@@ -34,17 +38,19 @@ class MyGame {
     // Step  B: Create the Renderable objects:
     this.mWhiteSq = new engine.Renderable();
     this.mWhiteSq.setColor([1, 1, 1, 1]);
+
     this.mRedSq = new engine.Renderable();
     this.mRedSq.setColor([1, 0, 0, 1]);
+    // this.mWhiteSq.getXform().setPosition(20, 60);
 
     // Step  C: Initialize the white Renderable object: centered, 5x5, rotated
     this.mWhiteSq.getXform().setPosition(20, 60);
-    this.mWhiteSq.getXform().setRotationInRad(0.2); // In Radians
-    this.mWhiteSq.getXform().setSize(5, 5);
+    // this.mWhiteSq.getXform().setRotationInRad(0.2); // In Radians
+    this.mWhiteSq.getXform().setSize(5,5);
 
     // Step  D: Initialize the red Renderable object: centered 2x2
-    this.mRedSq.getXform().setPosition(20, 60);
-    this.mRedSq.getXform().setSize(2, 2);
+    this.mRedSq.getXform().setPosition(50, 37.5);
+    this.mRedSq.getXform().setSize(1, 1);
   }
 
   // This is the draw function, make sure to setup proper drawing environment, and more
@@ -57,41 +63,149 @@ class MyGame {
     this.mCamera.setViewAndCameraMatrix();
 
     // Step  C: Activate the white shader to draw
-    this.mWhiteSq.draw(this.mCamera);
 
+    // if (engine.input.isKeyPressed(engine.input.keys.Space)) {
+    // engine.input.keys.Space = true;
+    // engine.isKeyPressed
+     /*if (this.spacebarPressed) {
+      this.mWhiteSq.draw(this.mCamera);
+
+     } */
+    // this.mWhiteSq.draw(this.mCamera);
+    // engine.input.keys.Space = 0;
+    // this.mWhiteSq.draw(this.mCamera);
+    // }
+    // this.mWhiteSq.draw(this.mCamera);s
+
+    if (this.spacebarPressed) {
+      for(let i =0; i < this.squares.length; i++) {
+        let pos = this.squarePositions[i];
+        this.squares[i].getXform().setPosition(pos.x,pos.y);
+        // this.squarePosition.setPosition(this.xPos[i], this.yPos[i]);
+
+      this.squares[i].draw(this.mCamera);
+      }
+      
+    }
+ 
     // Step  D: Activate the red shader to draw
     this.mRedSq.draw(this.mCamera);
+
+    requestAnimationFrame(this.draw.bind(this));
+  // setInterval(draw.bind(this), 10);
+
   }
+  //You can also use setInterval function in javascript
+  // to call the draw function in certain time interval as well.
+  // setInterval(draw.bind(this),10);
 
   // The update function, updates the application state. Make sure to _NOT_ draw
   // anything from this function!
   update() {
-    // For this very simple game, let's move the white square and pulse the red
-
-    let whiteXform = this.mWhiteSq.getXform();
-    let deltaX = 0.05;
-
-    // Step A: test for white square movement
-    if (engine.input.isKeyPressed(engine.input.keys.Right)) {
-      if (whiteXform.getXPos() > 30) {
-        // this is the right-bound of the window
-        whiteXform.setPosition(10, 60);
-      }
-      whiteXform.incXPosBy(deltaX);
-    }
-
-    // Step  B: test for white square rotation
-    if (engine.input.isKeyClicked(engine.input.keys.Up)) {
-      whiteXform.incRotationByDegree(1);
-    }
-
+    // for the red cursor
     let redXform = this.mRedSq.getXform();
-    // Step  C: test for pulsing the red square
-    if (engine.input.isKeyPressed(engine.input.keys.Down)) {
-      if (redXform.getWidth() > 5) {
-        redXform.setSize(2, 2);
+
+    let deltaX = 5;
+    let deltaX2 = -5;
+    let deltaY = 5;
+    let deltaY2 = -5;
+
+    // Step A: test for red square movement
+    if (engine.input.isKeyClicked(engine.input.keys.Right)) {
+      console.log(redXform.getXPos());
+
+      if (redXform.getXPos() > 100) {
+
+        // this is the right-bound of the window
+        redXform.setPosition(50, 37.5);
       }
-      redXform.incSizeBy(0.05);
+      redXform.incXPosBy(deltaX);
+    }
+
+    // test for red square movement
+    if (engine.input.isKeyClicked(engine.input.keys.Left)) {
+      console.log(redXform.getXPos());
+
+      if (redXform.getXPos() < 0) {
+        // this is the right-bound of the window
+        redXform.setPosition(50, 37.5);
+      }
+      redXform.incXPosBy(deltaX2);
+    }
+
+    // Step  B: test for Up key for red square
+    if (engine.input.isKeyClicked(engine.input.keys.Up)) {
+      console.log(redXform.getYPos());
+      if (redXform.getYPos() > 75) {
+        // this is the right-bound of the window
+        redXform.setPosition(50, 37.5);
+      }
+      redXform.incYPosBy(deltaY);
+    }
+
+    // Step  C: test for left down the red square
+    if (engine.input.isKeyClicked(engine.input.keys.Down)) {
+      console.log(redXform.getYPos());
+      if (redXform.getYPos() <0) {
+        // this is the right-bound of the window
+        redXform.setPosition(50, 37.5);
+      }
+      redXform.incYPosBy(deltaY2);
+    }
+
+    //for the random squares
+    // Step  D: test for space bar the red square
+    if (engine.input.isKeyClicked(engine.input.keys.Space)) {
+      //generate a random number between 10 and 20
+      let n = Math.floor(Math.random() * 11 + 10);
+
+      //Array to store the squares
+      this.squares = [];
+
+      //Array to store xPosition and yPosition of the squares
+      // this.xPos = [];
+      // this.yPos = [];
+
+      this.squarePositions = [];
+
+      //Loop to create n squares
+      for (let i = 0; i < n; i++) {
+        //Create a new square object
+        let square = new engine.Renderable();
+
+        // this.squarePositions.push({
+        //   x:
+        // })
+
+        //Set the center of the square to be within 5 units of the red cursor
+
+        let x = this.mRedSq.getXform().getXPos() + (Math.random() * 10 - 5);
+
+        let y = this.mRedSq.getXform().getYPos() + (Math.random() * 10 - 5);
+
+        this.squarePositions.push({ x: x, y: y });
+
+        // this.xPos.push(x);
+        // this.yPos.push(y);
+        // this.squarePosition = square.getXform;
+
+        // square.getXform().setPosition(x,y);
+
+        //Set the size of the sqaure to be a random number 1 and 6
+        let size = Math.random() * 5 + 1;
+        square.getXform().setSize(size, size);
+
+        //Set the square to be randomly rotated
+        square.getXform().setRotationInRad(Math.random() * 2 * Math.PI);
+
+        //Set the square to have a random color
+        square.setColor([Math.random(), Math.random(), Math.random(), 1]);
+        this.squares.push(square);
+      }
+
+    
+
+      this.spacebarPressed = true;
     }
   }
 }
